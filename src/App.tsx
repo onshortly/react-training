cb826b536ec447f1d6b42b4feeb361c6d0526083
Brent Baskin <bbaskin@kcftech.com>
2020-03-19 14:28:08 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Corporations from "./Corporations";
+
+export default function App() {
+  return <Corporations />;
+}
---
+++ src/Corporations.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Corporations() {
+  const corporations = [
+    { id: 1, name: "Apple", icon: "apple.png" },
+    { id: 2, name: "IBM", icon: "ibm.png" },
+    { id: 3, name: "Amazon", icon: "amazon.png" }
+  ];
+  return (
+    <>
+      <h1>Corporations</h1>
+    </>
+  );
+}
